5305eeb797fc609f35f5bc7864ebc78a372ca2cf
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-19 16:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your code here
 let nameInput = document.getElementById('item-name-input');
 let priceInput = document.getElementById('item-price-input');
+let qtyInput = document.getElementById('item-qty-input');
 let button = document.getElementById('add');
 let table = document.getElementById('myTable');
 let rowIndex = 1;
@@ -20,14 +21,17 @@ button.addEventListener('click', function(){
 
 	let cell1 = newRow.insertCell(0);
 	let cell2 = newRow.insertCell(1);
+	let cell3 = newRow.insertCell(2);
 
 	
 
 	cell1.innerHTML = nameInput.value;
-	cell2.innerHTML = priceInput.value;
+	cell2.innerHTML = qtyInput.value;
+	cell3.innerHTML = priceInput.value;
 
 	nameInput.value = '';
 	priceInput.value = '';
+	qtyInput.value = '';
 
 grandSum();
 	
